44b1abe2b70899b3a467b1056e1551e43f404be7
Chandu Bam <np03cs4s220313@heraldcollege.edu.np>
2024-04-13 15:32:34 +0545

    Add products

--- node-app/index.js
+++ node-app/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 var jwt = require('jsonwebtoken')
 const mongoose = require("mongoose");
-
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
 const app = express();
+const upload = multer({ storage: storage })
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
@@ -21,6 +34,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   });
 
 const Users = mongoose.model("Users", { username: String, password: String });
+const Prodcuts = mongoose.model("Products", { bookName: String, bookDescription: String, bookPrice:String, bookCategory:String, bookImage: String })
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello World!</h1>`);
@@ -73,6 +87,38 @@ app.post("/login", (req, res) => {
     });
 });
 
+//add listing
+app.post('/add-product',upload.single('bookImage'),(req,res)=>{
+  console.log(req.body);
+  console.log(req.file.path);
+  const bookName = req.body.bookName;
+  const bookImage = req.file.path;
+  const bookPrice = req.body.bookPrice;
+  const bookDescription = req.body.bookDescription;
+  const bookCategory = req.body.bookCategory; 
+  const product = new Prodcuts({bookName,bookDescription,bookPrice,bookCategory,bookImage})
+  product.save()  
+   .then(()=>{
+    res.send({message: 'Listing Success'})
+   })
+   .catch(()=>{
+    res.send({message:'Server err'})
+   })
+})
+
+app.get('/get-products',(req,res)=>{
+  Prodcuts.find()
+  .then((result)=>{
+    console.log({result:"product data"})
+    res.send({message:'success',products:result})
+
+  })
+  .catch((err)=>{
+    res.send({message:'server err'})
+
+  })
+  
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
